c17d6dfa131590665a6eea136b316f70bcc5adbb
Björn Heyser <bh@bjoernheyser.de>
2018-09-24 15:48:29 +0200

    version increase to 1.0.0

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id                = 'tstresmatrix';
-$version           = '0.0.1';
+$version           = '1.0.0';
 $ilias_min_version = '5.3.0';
 $ilias_max_version = '5.4.999';
 $responsible       = 'Björn Heyser';
