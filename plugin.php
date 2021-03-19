d9406d9a34374a5f64f0db62c5248dd998d405a0
mjansen <mjansen@databay.de>
2021-03-19 07:37:38 +0100

    Change supported version

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'tstresmatrix';
 $version = '1.3.0';
 $ilias_min_version = '5.4.0';
-$ilias_max_version = '7.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'Björn Heyser / Michael Jansen';
 $responsible_mail = 'info@bjoernheyser.de / mjansen@databay.de';
